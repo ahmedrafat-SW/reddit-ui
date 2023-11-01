a6d08e794245a5a38bad80f4f8afa7ee3ca63d24
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2023-11-01 23:07:42 +0200

    Added Navegation bar and setup home page layout

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'reddit';
+  postsCount: number[] = Array(10).fill(0);
+}
